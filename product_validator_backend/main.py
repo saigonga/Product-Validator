218c631aa269f8427ca2dd209fdcac7e655ba525
saigonga <saigonga34@gmail.com>
2026-03-16 22:13:03 +0530

    Just connected frontend to backend

--- product_validator_backend/main.py
+++ product_validator_backend/main.py
@@ -2,8 +2,17 @@
 from pydantic import BaseModel
 import analyzer
 from scrapper import hackernews
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/')
 def root_url():
--- product_validator_frontend/src/pages/ProductValidator.tsx
+++ product_validator_frontend/src/pages/ProductValidator.tsx
@@ -10,10 +10,35 @@ import {
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { use, useState } from "react"
-export function ProductValidatorCard() {
-  const [productInfo, setProductInfo] = useState("");
+import { useEffect, useState } from "react"
 
+
+export function ProductValidatorCard() {
+
+  const [productInfo, setProductInfo] = useState("");
+  const [productLoading, setProductLoading] = useState(false);
+  const[validateResult,setValidateResult]= useState("");
+
+
+
+  const handleValidate = async()=>{
+    try{
+         setProductLoading(true)
+
+
+        const response = await fetch(`http://localhost:8000/validate?text=${encodeURIComponent(productInfo)}`);
+        const data = await response.json();
+        setValidateResult(data.validation_result);
+
+    }
+    catch(error){
+      console.error("Error validating product:", error);
+    
+    }
+    finally{
+      setProductLoading(false)
+    }
+  }
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       {/* product validator input card */}
@@ -37,20 +62,26 @@ export function ProductValidatorCard() {
                 onChange={(e)=> setProductInfo(e.target.value)}
                 required
               />
-              <Button type="submit" className="w-full mt-2">
+              <Button type="button" className="w-full mt-2" onClick={handleValidate}>
                 Validate
               </Button>
+              {validateResult &&(
+                <div  className="mt-4 p-4 bg-green-100 text-green-800 rounded">
+                  <h3 className="text-lg font-semibold">Validation Result:</h3>
+                  <p>{validateResult}</p>
+                </div>
+              )}
             </div>
           </form>
         </CardContent>
       </Card>
 
       {/* product validator results card */}
-      <Card className="w-full max-w-md shadow-lg mt-6">
+      {/* <Card className="w-full max-w-md shadow-lg mt-6">
         <CardHeader>
           <CardTitle>Validation Results</CardTitle>
         </CardHeader>
-      </Card>
+      </Card> */}
     </div>
   );
 }
